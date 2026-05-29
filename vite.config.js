54ac552cc11b85cae688c6d92ee1ffe46037d00d
Timothée Ville <timothee.ville25@gmail.com>
2026-05-29 20:48:01 +0200

    Move site files to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/2x2voix/',
+  base: '/',
   test: {
     environment: 'jsdom',
     globals: true,
