3360a31d4b4f989d567c5cfc9ced90820b88cd79
Timothée Ville <timothee.ville25@gmail.com>
2026-09-23 13:27:41 +0200

    Ajoute de nouvelles images pour la galerie et met à jour les entrées correspondantes dans data.js

--- src/data.js
+++ src/data.js
@@ -107,6 +107,8 @@ export const GALLERY = [
   { label: "Église de Thise",           layout: "wide", file: "Visuel-eglise-sans-texte.jpg" },
   { label: "Répétition dans le jardin", layout: "wide", file: "Jardin.jpg" },
   { label: "Rennes en voix",            layout: "wide", file: "rennes-en-voix-1.jpg" },
-  { label: "Rennes en voix",            layout: "tall", file: "rennes-en-voix-2.jpg" },
-  { label: "Rennes en voix", layout: "wide", file: "rennes-en-voix-3.jpg" },
+  { label: "Rennes en voix",            layout: "wide", file: "rennes-en-voix-2.jpg" },
+  { label: "Rennes en voix",            layout: "wide", file: "rennes-en-voix-3.jpg" },
+  { label: "Chariez",                   layout: "tall", file: "chariez-1.jpg" },
+  { label: "Chariez",                   layout: "wide", file: "chariez-2.jpg" },
 ];
